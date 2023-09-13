acd12f3dc6909c7ec64dc800fd77ea35adbb9595
Yura Dubyk <dubykjurij@gmail.com>
2023-09-14 01:25:34 +0300

    add animations and adapted to ultra wide screen

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-const HIDE_CLASSNAME= "hide";
+const HIDE_CLASSNAME = "hide";
 const OPEN_CLASSNAME = "open";
 
 const navLinks = document.querySelector("#header__nav");
 const loginLinks = document.querySelector("#header__login");
-const registerLinks =document.querySelector("#header__register");
-const logoLinks =document.querySelector("#header__logo");
-
+const registerLinks = document.querySelector("#header__register");
+const logoLinks = document.querySelector("#header__logo");
 
 function toggleMenu() {
   if (navLinks.classList.contains(OPEN_CLASSNAME)) {
@@ -20,3 +19,22 @@ function toggleMenu() {
     logoLinks.classList.add(HIDE_CLASSNAME);
   }
 }
+
+// Клік поза випадаючим меню, закриття меню
+document.addEventListener("click", function (e) {
+  const dropdown = document.querySelector(".dropdown");
+  if (!dropdown.contains(e.target)) {
+    const dropdownContent = dropdown.querySelector(".dropdown-content");
+    dropdownContent.style.display = "none";
+  }
+});
+
+const dropdownBtn = document.querySelector(".hero__buttons");
+dropdownBtn.addEventListener("click", function () {
+  const dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+});
