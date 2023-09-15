fdb374819fa0ac218ccf577121142d48826c5f49
Yura Dubyk <dubykjurij@gmail.com>
2023-09-15 22:36:12 +0300

    update burger

--- js/main.js
+++ js/main.js
@@ -2,21 +2,24 @@ const HIDE_CLASSNAME = "hide";
 const OPEN_CLASSNAME = "open";
 
 const navLinks = document.querySelector("#header__nav");
-const loginLinks = document.querySelector("#header__login");
-const registerLinks = document.querySelector("#header__register");
-const logoLinks = document.querySelector("#header__logo");
+
+const menuOpenLinks = document.querySelector("#header__menu__open");
+const menuCloseLinks = document.querySelector("#header__menu__close");
+const bodyLinks = document.querySelector("body");
 
 function toggleMenu() {
   if (navLinks.classList.contains(OPEN_CLASSNAME)) {
     navLinks.classList.remove(OPEN_CLASSNAME);
-    loginLinks.classList.remove(HIDE_CLASSNAME);
-    registerLinks.classList.remove(HIDE_CLASSNAME);
-    logoLinks.classList.remove(HIDE_CLASSNAME);
+    menuCloseLinks.classList.remove(OPEN_CLASSNAME);
+    bodyLinks.classList.remove(OPEN_CLASSNAME);
+    menuOpenLinks.classList.remove(OPEN_CLASSNAME);
+    navLinks.classList.add(HIDE_CLASSNAME);
   } else {
     navLinks.classList.add(OPEN_CLASSNAME);
-    loginLinks.classList.add(HIDE_CLASSNAME);
-    registerLinks.classList.add(HIDE_CLASSNAME);
-    logoLinks.classList.add(HIDE_CLASSNAME);
+    menuCloseLinks.classList.add(OPEN_CLASSNAME);
+    bodyLinks.classList.add(OPEN_CLASSNAME);
+    menuOpenLinks.classList.add(OPEN_CLASSNAME);
+    navLinks.classList.remove(HIDE_CLASSNAME);
   }
 }
 
