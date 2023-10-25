4c241e6823a4197951bff7aac6249734f0842281
Yura Dubyk <dubykjurij@gmail.com>
2023-10-26 01:35:42 +0300

    add filter menu

--- src/components/CatalogFilter/CatalogFilter.js
+++ src/components/CatalogFilter/CatalogFilter.js
@@ -1,16 +1,35 @@
 import { Inner, Wrapper } from "../CommonComponenst";
 import { Dropdown } from "rsuite";
-import { MyDropdown } from "./CatalogFilter.styled";
+import {
+  MyButton,
+  MyDropdown,
+  SortedBy,
+  MyLabel,
+} from "./CatalogFilter.styled";
 
 const CatalogFilter = () => {
   return (
     <Wrapper style={{ borderRadius: "12px" }}>
-      <Inner style={{justifyContent:"flex-start"}}>
-        <MyDropdown title="Filter">
-          <Dropdown.Item>Sort by price</Dropdown.Item>
-          <Dropdown.Item>Sort by title</Dropdown.Item>
-          <Dropdown.Item>Sort by weight</Dropdown.Item>
-        </MyDropdown>
+      <Inner style={{ justifyContent: "space-between", marginBottom: "20px" }}>
+        <div style={{ display: "flex" }}>
+          <MyDropdown title="Filter">
+            <Dropdown.Item>Sort by price</Dropdown.Item>
+            <Dropdown.Item>Sort by title</Dropdown.Item>
+            <Dropdown.Item>Sort by weight</Dropdown.Item>
+          </MyDropdown>
+          <SortedBy>
+            <a>Bikes sorted by: title/name/weight</a>
+          </SortedBy>
+        </div>
+        <div style={{ display: "flex" }}>
+          <MyLabel>
+            Minimum price <input name="minPrice" />
+          </MyLabel>
+          <MyLabel>
+            Maximum price <input name="MaxPrice" />
+          </MyLabel>
+        </div>
+        <MyButton><a>Apply</a></MyButton>
       </Inner>
     </Wrapper>
   );
--- src/components/CatalogFilter/CatalogFilter.styled.js
+++ src/components/CatalogFilter/CatalogFilter.styled.js
@@ -2,8 +2,7 @@ import styled from "styled-components";
 import { Dropdown } from "rsuite";
 
 export const MyDropdown = styled(Dropdown)`
-  margin-bottom: 20px;
-  
+
   button {
     font-size: 1.3vw;
     color: black;
@@ -12,6 +11,9 @@ export const MyDropdown = styled(Dropdown)`
     border-radius: 12px;
     background-color: #e6e6e6;
     box-shadow: 2px 3px 5px #bbbbbb;
+    margin-right: 1.4vw;
+    max-height: 2.3vw;
+
   }
 
   button:hover {
@@ -20,31 +22,83 @@ export const MyDropdown = styled(Dropdown)`
   }
 
   ul {
-    background-color:#e6e6e6;
+    background-color: #e6e6e6;
     position: absolute;
     z-index: 999;
     list-style: none;
     padding-left: 0;
     flex-direction: column;
     justify-content: center;
-    border:1px solid #c2c2c2;
-    border-radius:9px;
-    padding:8px 5px 8px 5px;
-    margin-top:3px;
-    width:8.1vw;
-    text-align:center;
+    border: 1px solid #c2c2c2;
+    border-radius: 9px;
+    padding: 8px 5px 8px 5px;
+    margin-top: 3px;
+    width: 8.1vw;
+    text-align: center;
     box-shadow: 2px 3px 5px #bbbbbb;
   }
 
   li {
-    border-radius:8px;
+    border-radius: 8px;
     text-decoration: none;
-    border:1px solid black;
+    border: 1px solid black;
     background-color: lightgray;
-    margin-bottom:4px;
+    margin-bottom: 4px;
     cursor: pointer;
-    font-size:14px;
+    font-size: 14px;
     padding-top: 3px;
     padding-bottom: 3px;
+  }
+`;
+
+export const MyButton = styled.button`
+  display: flex;
+  align-items: center;
+  font-size: 1.3vw;
+  color: black;
+  padding: 0 2.2vw;
+  border: 1px solid #d6d6d6;
+  border-radius: 12px;
+  background-color: #e6e6e6;
+  box-shadow: 2px 3px 5px #bbbbbb;
+  margin-right: 1.4vw;
+  max-height: 2.3vw;
+  cursor:pointer;
+`;
+
+export const SortedBy = styled.label`
+  display: flex;
+  align-items: center;
+  font-size: 1.3vw;
+  color: black;
+  padding: 0 2.2vw;
+  border: 1px solid #d6d6d6;
+  border-radius: 12px;
+  background-color: #f0f0f0;
+  box-shadow: 2px 3px 5px #bbbbbb;
+  margin-right: 1.4vw;
+  max-height: 2.3vw;
+  a {
+  }
+`;
+
+export const MyLabel = styled.label`
+  display: flex;
+  align-items: center;
+  font-size: 1.1vw;
+  border: 1px solid #d6d6d6;
+  border-radius: 12px;
+  background-color: #f0f0f0;
+  box-shadow: 2px 3px 5px #bbbbbb;
+  padding: 0 1.2vw;
+  max-height: 2.3vw;
+  margin-right:1.4vw;
+  input {
+    padding: 0;
+    border: 1px solid grey;
+    border-radius: 4px;
+    margin-left: 6px;
+    width: 7vw;
+    max-height: 1.6vw;
   }
 `;
