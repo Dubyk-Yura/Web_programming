f74143e9cf0f981d49a80b1f363b217b2de3164a
Yura Dubyk <dubykjurij@gmail.com>
2023-10-27 01:48:57 +0300

    change dropdown button

--- src/components/CatalogFilter/CatalogFilter.js
+++ src/components/CatalogFilter/CatalogFilter.js
@@ -1,26 +1,26 @@
 import { Inner, Wrapper } from "../CommonComponenst";
-import { Dropdown } from "rsuite";
 import {
   MyButton,
-  MyDropdown,
-  SortedBy,
   MyLabel,
+  SelectSort,
 } from "./CatalogFilter.styled";
 
 const CatalogFilter = () => {
   return (
-    <Wrapper style={{ borderRadius: "12px", maxWidth:"97%" }}>
-      <Inner style={{ justifyContent: "space-between", marginBottom: "20px" }}>
-        <div style={{ display: "flex" }}>
-          <MyDropdown title="Filter">
-            <Dropdown.Item>Sort by price</Dropdown.Item>
-            <Dropdown.Item>Sort by title</Dropdown.Item>
-            <Dropdown.Item>Sort by weight</Dropdown.Item>
-          </MyDropdown>
-          <SortedBy>
-            <text>Bikes sorted by: title/name/weight</text>
-          </SortedBy>
-        </div>
+    <Wrapper style={{ borderRadius: "12px", maxWidth: "97%" }}>
+      <Inner
+        style={{
+          justifyContent: "space-between",
+          marginBottom: "1.4vw",
+          marginTop: "1.4vw",
+        }}
+      >
+        <SelectSort name="question">
+          <option style={{ display: "none" }}>Choose option</option>
+          <option value="sortByPrice">Sort by price</option>
+          <option value="sortByTitle">Sort by title</option>
+          <option value="sortByWeight">Sort by weight</option>
+        </SelectSort>
         <div style={{ display: "flex" }}>
           <MyLabel>
             Minimum price <input type="number" name="minPrice" />
@@ -29,7 +29,9 @@ const CatalogFilter = () => {
             Maximum price <input type="number" name="MaxPrice" />
           </MyLabel>
         </div>
-        <MyButton><text>Apply</text></MyButton>
+        <MyButton>
+          <text>Apply</text>
+        </MyButton>
       </Inner>
     </Wrapper>
   );
--- src/components/CatalogFilter/CatalogFilter.styled.js
+++ src/components/CatalogFilter/CatalogFilter.styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
-import { Dropdown } from "rsuite";
 
-const buttonStyles = `
+export const MyButton = styled.button`
   font-size: 1.3vw;
   color: black;
   padding: 0.4vw 2.2vw;
@@ -12,60 +11,9 @@ const buttonStyles = `
   margin-right: 1.4vw;
   max-height: 2.3vw;
   transition: background-color 0.35s ease, border-color 0.35s ease;
-`;
-
-export const MyDropdown = styled(Dropdown)`
-  button {
-    ${buttonStyles}
-  }
-
-  button:hover {
-    background-color: #c2c2c2;
-    border-color: #b3b3b3;
-  }
-
-  ul {
-    background-color: #e6e6e6;
-    position: absolute;
-    z-index: 999;
-    list-style: none;
-    padding-left: 0;
-    flex-direction: column;
-    justify-content: center;
-    border: 1px solid #c2c2c2;
-    border-radius: 9px;
-    padding: 8px 5px 8px 5px;
-    margin-top: 3px;
-    width: 8.1vw;
-    text-align: center;
-    box-shadow: 2px 3px 5px #bbbbbb;
-  }
-
-  li {
-    border-radius: 8px;
-    text-decoration: none;
-    border: 1px solid black;
-    background-color: lightgray;
-    margin-bottom: 4px;
-    cursor: pointer;
-    font-size: 1vw;
-    padding-top: 3px;
-    padding-bottom: 3px;
-  }
-`;
-
-export const MyButton = styled.button`
-  ${buttonStyles}
   display: flex;
   align-items: center;
   cursor: pointer;
-`;
-
-export const SortedBy = styled.label`
-  ${buttonStyles}
-  display: flex;
-  align-items: center;
-  background-color: #f0f0f0;
 `;
 
 export const MyLabel = styled.label`
@@ -90,4 +38,19 @@ export const MyLabel = styled.label`
     max-height: 30%;
     min-height: 45%;
   }
+`;
+
+export const SelectSort = styled.select`
+  appearance: none;
+  -webkit-appearance: none;
+  font-size: 1.3vw;
+  color: black;
+  padding: 0.4vw 2.2vw;
+  border: 1px solid #d6d6d6;
+  border-radius: 12px;
+  background-color: #e6e6e6;
+  box-shadow: 2px 3px 5px #bbbbbb;
+  margin-right: 1.4vw;
+  max-height: 2.3vw;
+  transition: background-color 0.35s ease, border-color 0.35s ease;
 `;
